d0f71175d67e581efebcba6db208c94cf6fdc351
SHABAN171 <shabanindekupe@gmail.com>
2026-05-22 20:41:28 +0300

    feat: settings and requirements files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-change-this-in-production-use-env-var'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -97,4 +97,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
 ]
-CORS_ALLOW_ALL_ORIGINS = DEBUG
+CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
