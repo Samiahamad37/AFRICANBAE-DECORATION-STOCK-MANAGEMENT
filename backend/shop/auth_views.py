1ce9dacc83432bff8b688ea17750fa3f16d15c0c
Samia <hamadsamia37@gmail.com>
2026-06-01 22:25:08 +0300

    correcting the importing of serializer

--- backend/shop/auth_views.py
+++ backend/shop/auth_views.py
@@ -12,7 +12,147 @@ from .auth_serializers import (
     PasswordResetSerializer,
     PasswordResetConfirmSerializer
 )
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
+from django.utils.http import (
+    urlsafe_base64_encode,
+    urlsafe_base64_decode
+)
+
+from django.utils.encoding import (
+    smart_bytes,
+    force_str
+)
+
+from django.core.mail import send_mail
+from django.conf import settings
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+
+
+class RequestPasswordResetView(APIView):
+
+    def post(self, request):
+
+        serializer = PasswordResetSerializer(
+            data=request.data
+        )
+
+        serializer.is_valid(raise_exception=True)
+
+        email = serializer.validated_data['email']
+
+        try:
+
+            user = User.objects.get(email=email)
+
+            uidb64 = urlsafe_base64_encode(
+                smart_bytes(user.id)
+            )
+
+            token = PasswordResetTokenGenerator().make_token(user)
+
+            reset_link = (
+                f"http://localhost:3000/"
+                f"reset-password/{uidb64}/{token}"
+            )
+
+            send_mail(
+                subject='Reset Your Password',
+
+                message=f'''
+Hello {user.username},
+
+Click this link to reset your password:
+
+{reset_link}
+                ''',
+
+                from_email=settings.DEFAULT_FROM_EMAIL,
+
+                recipient_list=[email],
+
+                fail_silently=False,
+            )
+
+            return Response(
+                {
+                    "success": True,
+                    "message": "Password reset email sent"
+                },
+                status=status.HTTP_200_OK
+            )
+
+        except User.DoesNotExist:
+
+            return Response(
+                {
+                    "success": False,
+                    "message": "User not found"
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+
+class PasswordResetConfirmView(APIView):
+
+    def post(self, request, uidb64, token):
+
+        serializer = PasswordResetConfirmSerializer(
+            data=request.data
+        )
+
+        serializer.is_valid(raise_exception=True)
+
+        try:
+
+            user_id = force_str(
+                urlsafe_base64_decode(uidb64)
+            )
+
+            user = User.objects.get(id=user_id)
+
+            if not PasswordResetTokenGenerator().check_token(
+                user,
+                token
+            ):
+
+                return Response(
+                    {
+                        "success": False,
+                        "message": "Invalid token"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            user.set_password(
+                serializer.validated_data['new_password']
+            )
+
+            user.save()
+
+            return Response(
+                {
+                    "success": True,
+                    "message": "Password reset successful"
+                },
+                status=status.HTTP_200_OK
+            )
+
+        except Exception:
+
+            return Response(
+                {
+                    "success": False,
+                    "message": "Something went wrong"
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
 class RegisterView(viewsets.ViewSet):
     permission_classes = [AllowAny]
