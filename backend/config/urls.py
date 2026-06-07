832d8f90b38cf01714c056a4f74cefb80f2df75b
SHABAN171 <shabanindekupe@gmail.com>
2026-06-07 18:37:12 +0300

    image display test in main urls.py file

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,14 +4,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
 
+
 def home(request):
     return JsonResponse({
         "message": "AfricanBae Backend API Running"
     })
+
 
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('shop.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
