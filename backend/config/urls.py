a42bd9cc7c796a1abaec630947aa4bc1cf894e8e
SHABAN171 <shabanindekupe@gmail.com>
2026-06-07 23:37:18 +0300

    updated main urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('shop.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
