949cb08c96df5e1607bc07e84bca76779e79cb0b
SHABAN171 <shabanindekupe@gmail.com>
2026-06-09 20:26:31 +0300

    modifying cloudinary storage setup

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -90,9 +90,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': config('CLOUDINARY_API_KEY'),
-    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
