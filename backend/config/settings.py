517e5a304c33b6f075b0f552107fbc0fc78fc1dd
Samia <hamadsamia37@gmail.com>
2026-06-10 21:08:14 +0300

    removing unneccessarly lines of code

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -57,7 +57,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': config('CLOUDINARY_API_KEY'),
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
-    'SECURE': True,   # 🔥 ADD THIS LINE
+    'SECURE': True,   
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
