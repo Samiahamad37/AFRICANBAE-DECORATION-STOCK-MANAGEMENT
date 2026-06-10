c18810926dbf053e395b6c6fa44afb18826a52be
SHABAN171 <shabanindekupe@gmail.com>
2026-06-10 10:59:16 +0300

    updated productcard with security url

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -16,7 +16,10 @@ export default function ProductCard({
   } = product;
 
   // Use the full URL from the backend
-  const img = product.image_url || product.image || null;
+  const img = 
+  product.image_url?.replace("http://", "https://") || 
+   product.image ||
+    null;
 
   console.log("Product ID:", product.id);
   console.log("Product Name:", name);
