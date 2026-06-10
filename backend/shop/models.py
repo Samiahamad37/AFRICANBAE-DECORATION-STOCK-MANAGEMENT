54e7bb234f7aaa388e5369cd0032f87e6807f008
SHABAN171 <shabanindekupe@gmail.com>
2026-06-10 09:08:33 +0300

    updated models.py to allow cloudinary image storage

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    image = CloudinaryField('image', blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    # image = models.ImageField(upload_to='products/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
