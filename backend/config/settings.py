970bddec8210423a5f05906e04b231fb4d4e95e0
SHABAN171 <shabanindekupe@gmail.com>
2026-06-19 19:43:53 +0300

    email timeout module was added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,6 +32,8 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
+EMAIL_TIMEOUT = 10
+
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@africanbae.com')
 
 FRONTEND_URL = os.getenv(
