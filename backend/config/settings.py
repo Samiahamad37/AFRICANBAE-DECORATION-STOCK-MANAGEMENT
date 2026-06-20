ee7f651cc7833db166c8cb32fb10c2d965954b12
SHABAN171 <shabanindekupe@gmail.com>
2026-06-20 17:59:43 +0300

    Add email debug logs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,7 +34,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 EMAIL_TIMEOUT = 10
 
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@africanbae.com')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'hamadsamia37@gmail.com')
 
 FRONTEND_URL = os.getenv(
     'FRONTEND_URL',
